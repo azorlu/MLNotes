5d13d99582554e6913a8af192abdb30c3c45f1a0
azorlu <mandelbug@gmail.com>
2019-03-23 19:29:23 +0900

    VSCode setup

--- MLNotes.py
+++ MLNotes.py
@@ -1,3 +1,12 @@
+#%% [markdown]
+# __Machine Learning Notes__
+
 #%%
-msg = "Hello World"
-print(msg)
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+x = np.linspace(0, 30, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+
